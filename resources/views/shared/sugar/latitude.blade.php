435cd1389d3ad16f6845f1d640bb7a3ac61f1e8b
adamakbar <adamakbar@yandex.com>
2016-01-19 23:15:32 +0700

    Latitude form component clean up

--- resources/views/shared/sugar/latitude.blade.php
+++ resources/views/shared/sugar/latitude.blade.php
@@ -2,23 +2,23 @@
   || $errors->has(squareToDot($name).'.minute')
   || $errors->has(squareToDot($name).'.second')
   || $errors->has(squareToDot($name).'.cardinal'))
-<div class="form-group has-feedback has-error">
+  <div class="form-group has-feedback has-error">
 @else
-<div class="form-group">
+  <div class="form-group">
 @endif
 
   @if ($required)
-  <label for="{{ $name }}" class="col-md-3 control-label required">
+    <label for="{{ $name }}" class="col-md-3 control-label required">
   @else
-  <label for="{{ $name }}" class="col-md-3 control-label">
+    <label for="{{ $name }}" class="col-md-3 control-label">
   @endif
     Center latitude
   </label>
 
   @if ($errors->has(squareToDot($name).'.degree'))
-  <div class="col-md-2 has-feedback has-error">
+    <div class="col-md-2 has-feedback has-error">
   @else
-  <div class="col-md-2">
+    <div class="col-md-2">
   @endif
 
     <div class="input-group">
@@ -27,17 +27,17 @@
     </div>
 
     @if ($errors->has(squareToDot($name).'.degree'))
-    <span class="form-control-feedback glyphicon glyphicon-remove"
-          style="right: -15px;"></span>
-    <span id="{{ $name.'[degree]' }}" class="sr-only">(error)</span>
-    <p class="help-block">{{ $errors->first(squareToDot($name).'.degree') }}</p>
+      <span class="form-control-feedback glyphicon glyphicon-remove"
+            style="right: -15px;"></span>
+      <span id="{{ $name.'[degree]' }}" class="sr-only">(error)</span>
+      <p class="help-block">{{ $errors->first(squareToDot($name).'.degree') }}</p>
     @endif
   </div>
 
   @if ($errors->has(squareToDot($name).'.minute'))
-  <div class="col-md-2 has-feedback has-error">
+    <div class="col-md-2 has-feedback has-error">
   @else
-  <div class="col-md-2">
+    <div class="col-md-2">
   @endif
 
     <div class="input-group">
@@ -46,17 +46,17 @@
     </div>
 
     @if ($errors->has(squareToDot($name).'.minute'))
-    <span class="form-control-feedback glyphicon glyphicon-remove"
-          style="right: -15px;"></span>
-    <span id="{{ $name.'[minute]' }}" class="sr-only">(error)</span>
-    <p class="help-block">{{ $errors->first(squareToDot($name).'.minute') }}</p>
+      <span class="form-control-feedback glyphicon glyphicon-remove"
+            style="right: -15px;"></span>
+      <span id="{{ $name.'[minute]' }}" class="sr-only">(error)</span>
+      <p class="help-block">{{ $errors->first(squareToDot($name).'.minute') }}</p>
     @endif
   </div>
 
   @if ($errors->has(squareToDot($name).'.second'))
-  <div class="col-md-2 has-feedback has-error">
+    <div class="col-md-2 has-feedback has-error">
   @else
-  <div class="col-md-2">
+    <div class="col-md-2">
   @endif
 
     <div class="input-group">
@@ -65,26 +65,26 @@
     </div>
 
     @if ($errors->has(squareToDot($name).'.second'))
-    <span class="form-control-feedback glyphicon glyphicon-remove"
-          style="right: -15px;"></span>
-    <span id="{{ $name.'[second]' }}" class="sr-only">(error)</span>
-    <p class="help-block">{{ $errors->first(squareToDot($name).'.second') }}</p>
+      <span class="form-control-feedback glyphicon glyphicon-remove"
+            style="right: -15px;"></span>
+      <span id="{{ $name.'[second]' }}" class="sr-only">(error)</span>
+      <p class="help-block">{{ $errors->first(squareToDot($name).'.second') }}</p>
     @endif
   </div>
 
   @if ($errors->has(squareToDot($name).'.cardinal'))
-  <div class="col-md-2 has-feedback has-error">
+    <div class="col-md-2 has-feedback has-error">
   @else
-  <div class="col-md-2">
+    <div class="col-md-2">
   @endif
 
     {{ Form::text($name.'[cardinal]', null, ['class' => 'form-control', 'placeholder' => 'S/N']) }}
 
     @if ($errors->has(squareToDot($name).'.cardinal'))
-    <span class="form-control-feedback glyphicon glyphicon-remove"
-          style="right: -15px;"></span>
-    <span id="{{ $name.'[cardinal]' }}" class="sr-only">(error)</span>
-    <p class="help-block">{{ $errors->first(squareToDot($name).'.cardinal') }}</p>
+      <span class="form-control-feedback glyphicon glyphicon-remove"
+            style="right: -15px;"></span>
+      <span id="{{ $name.'[cardinal]' }}" class="sr-only">(error)</span>
+      <p class="help-block">{{ $errors->first(squareToDot($name).'.cardinal') }}</p>
     @endif
   </div>
 </div>
